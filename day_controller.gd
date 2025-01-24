819ef51adfa5b9be9949640c2fb75f6aad686949
Davi Silva <davi.silva@gendigital.com>
2025-01-24 20:32:54 +0100

    Pressing enter to move to next day.

--- day_controller.gd
+++ day_controller.gd
@@ -1,10 +1,22 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var starting_day = 0
+var current_day = 0
+var max_days = 7
 
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		advance_day()
+
+func advance_day() -> void:
+	if (starting_day < max_days):
+		print("Going to next day..")
+		starting_day += 1
+	else:
+		print("Finish the run..")
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
